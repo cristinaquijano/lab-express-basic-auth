1b1cf0bfd35043fd4842f7d27aaa8374aab1c60a
Cristina Quijano <cristina@cristinas-air-2.kpn>
2022-04-28 09:07:04 +0200

    iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const {isLoggedIn} = require("../middleware/routeGuard")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/private", isLoggedIn, (req,res) => {
+  res.render("private", {user: req.session.currentUser})
+})
 
 module.exports = router;
